d1a0ed8838eb0d1764cc67db34b3697b4e36eaf1
hitanshu-mehta <hitanshu99amehta@gmail.com>
2020-11-18 16:28:01 +0530

    Added special characters in character sets

--- randomGenerator.js
+++ randomGenerator.js
@@ -8,11 +8,12 @@ const MAX_UINT32 = Math.pow(2, 32) - 1;
 
 const LOWER_CASE = "abcdefghijklmnopqrstuvwxyz";
 const UPPER_CASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const SPECAIL_CASE = "$#@!%^&*()+-,/:;<>[]_~|{}`'?";
 const DIGITS = "0123456789";
 
-const REQUIRED_CHARACTER_SETS = [LOWER_CASE,UPPER_CASE,DIGITS];
+const REQUIRED_CHARACTER_SETS = [LOWER_CASE, UPPER_CASE, DIGITS, SPECAIL_CASE];
 
-this.passwordGenerator = {
+export const passwordGenerator = {
 
     /**
      * Randomly generates password of given length (if not -1)
@@ -31,8 +32,8 @@ this.passwordGenerator = {
      */
     generatePassword(length = DEFAULT_PASSWORD_LENGTH) {
 
-        if(length === -1){
-            length = DEFAULT_PASSWORD_MIN_LENGTH + this._randomIndex(DEFAULT_PASSWORD_LENGTH - DEFAULT_PASSWORD_MIN_LENGTH + 1) 
+        if (length === -1) {
+            length = DEFAULT_PASSWORD_MIN_LENGTH + this._randomIndex(DEFAULT_PASSWORD_LENGTH - DEFAULT_PASSWORD_MIN_LENGTH + 1)
         }
 
         if (length < DEFAULT_PASSWORD_MIN_LENGTH) {
@@ -48,17 +49,18 @@ this.passwordGenerator = {
         // randomly select  number of lower case letters, upper case letters and digits such that atleast one from each
         // group exists in the password.
 
-        let lengthLower = this._randomIndex(length-3) + 1; 
-        let lengthUpper = this._randomIndex(length-lengthLower-2) + 1;
-        let lengthDigit = length - lengthLower - lengthUpper;
+        let lengthLower = this._randomIndex(length - 3) + 1;
+        let lengthUpper = this._randomIndex(length - lengthLower - 2) + 1;
+        let lengthDigit = this._randomIndex(length - lengthLower - lengthUpper - 2) + 1;
+        let lengthSpecial = length - lengthLower - lengthUpper - lengthDigit;
 
-        let lenghtOfRequiredCharacterSets = [lengthLower,lengthUpper,lengthDigit];
+        let lengthOfRequiredCharacterSets = [lengthLower, lengthUpper, lengthDigit, lengthSpecial];
 
 
-        for(const [idx,charSetString] of REQUIRED_CHARACTER_SETS.entries()){
-            while(lenghtOfRequiredCharacterSets[idx]--){
+        for (const [idx, charSetString] of REQUIRED_CHARACTER_SETS.entries()) {
+            while (lengthOfRequiredCharacterSets[idx]--) {
                 password += charSetString[this._randomIndex(charSetString.length)];
-            }   
+            }
         }
 
         password = this._shuffleString(password);
@@ -73,20 +75,20 @@ this.passwordGenerator = {
      * @returns a random number in the range [0,range)
      * @throws Error if 'range' cannot fit in uint8
      */
-    _randomIndex(range){
-        if(range > MAX_UINT8){
+    _randomIndex(range) {
+        if (range > MAX_UINT8) {
             throw new Error("`range` cannot fit into uint8");
         }
-        
+
 
         // we must discard random number above this value, otherwise it will make 
         // random number generator non-uniform
         const MAX_ACCEPTABLE_VALUE = Math.floor(MAX_UINT8 / range) * range - 1;
 
         const randomValueArr = new Uint16Array(1);
-        do{
+        do {
             window.crypto.getRandomValues(randomValueArr);
-        } while(randomValueArr[0] > MAX_ACCEPTABLE_VALUE);
+        } while (randomValueArr[0] > MAX_ACCEPTABLE_VALUE);
 
         return randomValueArr[0] % range;
     },
@@ -106,11 +108,11 @@ this.passwordGenerator = {
         // Generate all the random numbers that will be needed
         const randomValues = new Uint32Array(arr.length - 1);
         crypto.getRandomValues(randomValues);
-        
+
         for (let i = arr.length - 1; i > 0; i--) {
             const j = Math.floor((randomValues[i - 1] / MAX_UINT32) * (i + 1));
             [arr[i], arr[j]] = [arr[j], arr[i]];
-          }
+        }
         return arr.join("");
     }
 
