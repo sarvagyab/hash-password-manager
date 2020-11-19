85c8b4bb0573b431b9f92dfa0b11ca73d5b908f0
hitanshu-mehta <hitanshu99amehta@gmail.com>
2020-11-19 08:56:29 +0530

    Add unit test for random password generator

--- tests.js
+++ tests.js
@@ -2,6 +2,7 @@ import { deriveMasterKey } from './generateMasterKey.js';
 import { AESdecrypt, AESencrypt } from './AESUtils.js';
 import CryptoJS from 'crypto-js';
 
+import { passwordGenerator } from './randomGenerator'
 
 verifyTests();
 
@@ -10,6 +11,7 @@ function verifyTests() {
     let working = true;
     working &= deriveMasterKeyTesting();
     working &= AESencryptionTesting();
+    working &= RandomPasswordGeneratorTesting();
     if (working) console.log("All Functions are working correctly");
     else console.log("Something's wrong")
 }
@@ -29,7 +31,7 @@ function deriveMasterKeyTesting(password = 'myNameIsSarvagya') {
 function AESencryptionTesting() {
     const passphrase = "Loreum Ipsum";
     const masterKey = deriveMasterKey('myNameIsSarvagya');
-    const encryptedKey = masterKey.encryptionKey.substr(0,masterKey.encryptionKey.length/2);
+    const encryptedKey = masterKey.encryptionKey.substr(0, masterKey.encryptionKey.length / 2);
     const encrypted = AESencrypt(passphrase, encryptedKey);
     const decrytped = AESdecrypt(encrypted.cipherText, encryptedKey, encrypted.iv);
     const result = (passphrase === decrytped);
@@ -37,3 +39,56 @@ function AESencryptionTesting() {
     else console.log('AESencrypt/AESdecrypt - False');
     return result;
 }
+
+// Testing random password generator
+function RandomPasswordGeneratorTesting() {
+
+    // preparations
+    const LOWER_CASE = "abcdefghijklmnopqrstuvwxyz";
+    const UPPER_CASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const SPECIAL_CASE = "$#@!%^&*()+-,/:;<>[]_~|{}`'?";
+    const DIGITS = "0123456789";
+
+    const _countOccurance = (str) => {
+        let LL = 0, LU = 0, LS = 0, LD = 0;
+        for (const ch of str) {
+            if (LOWER_CASE.indexOf(ch) != -1) ++LL;
+            else if (UPPER_CASE.indexOf(ch) != -1) ++LU;
+            else if (DIGITS.indexOf(ch) != -1) ++LD;
+            else if (SPECIAL_CASE.indexOf(ch) != -1) ++LS;
+        }
+        return [LL, LU, LD, LS];
+    }
+
+    const _checkError = (func, expectedMsg, ...params) => {
+        try {
+            func.apply(this, params);
+            return false;
+        }
+        catch (err) {
+            return (expectedMsg == err.message);
+        }
+    }
+    //
+
+    // Check length of generated password
+    let password = passwordGenerator.generatePassword(10);
+    let result = (password.length == 10);
+
+    // Check correct number of characters are generated or not
+    password = passwordGenerator.generatePassword(10, 3, 4, 2, 1);
+    let fArray = _countOccurance(password);
+    result &= (fArray[0] <= 3 && fArray[1] <= 4 && fArray[2] <= 2 && fArray[3] <= 1);
+
+
+    // Checking Errors
+    result &= _checkError(passwordGenerator.generatePassword, 'Requested password length is too long', 1e10);
+    result &= _checkError(passwordGenerator.generatePassword, 'Requested password length is too short', 2);
+    result &= _checkError(passwordGenerator.generatePassword, 'minLengthLower + minLengthUpper + minLengthDigit + minLengthSpecial should be less than length', 10, 3, 4, 10, 3);
+
+    if (result) console.log('Random Password Generation - True');
+    else console.log('Random Password Generation - False');
+
+    return result;
+}
+
